5d81c5b12f9a55994f845561c1ea8fb549f7ae37
Salohiddin17 <salohiddindilmatov9@gmail.com>
2021-07-05 22:29:16 +0500

    dasdasd

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.status(200).json({
-    status: 200,
-    message: "Hit!",
-  });
+  res.send("Hello world")
 });
 
 app.listen(8080, () => console.log("Server running port 8080"));
